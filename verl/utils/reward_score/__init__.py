25b627a70089c52210452934e520a551a13e4f57
hoanganhpham1006 <hoanganh.pham.12327@gmail.com>
2025-04-04 03:19:32 +0000

    Accuracy reward simplified

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -41,6 +41,9 @@ def _default_compute_score(data_source, solution_str, ground_truth, extra_info=N
     elif data_source in ['hiyouga/geometry3k']:
         from . import geo3k
         res = geo3k.compute_score(solution_str, ground_truth)
+    elif data_source in ['hoanganhpham/openr1', 'hoanganhpham/aime24']:
+        from . import openr1
+        res = openr1.compute_score(solution_str, ground_truth)
     else:
         raise NotImplementedError
 
